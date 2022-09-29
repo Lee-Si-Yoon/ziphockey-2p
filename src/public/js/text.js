bee6606db9ebc14bdc5534d4c7eabce029df995f
leesiyoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2022-09-30 00:52:52 +0900

    애니메이션 버그 고침

--- src/public/js/text.js
+++ src/public/js/text.js
@@ -1,29 +1,32 @@
 const socket = io();
 
 const lerp = (a, b, n) => parseFloat((1 - n) * a + n + b).toFixed(2);
-
-let posY;
-let accel = 1;
+let rafId;
 
 export default class TextArea {
   constructor(ulElement, formElement) {
     this.textContainer = ulElement;
     this.form = formElement;
     this.startPosition = { x: 0, y: 0 };
+    this.velocity = 2;
+    this.accel = 0.25;
     this.onSubmit(this.form);
   }
 
   onSubmit(element) {
     element.addEventListener("submit", (e) => {
       e.preventDefault();
-      let textChildrens = [...this.textContainer.children];
+      let textChildrens = [
+        ...document.getElementById("textAreaContainer").children,
+      ];
       if (textChildrens.length === 0) {
         // 새로 메세지를 보낼 경우
         this.createBox(e.target[0].value);
         e.target[0].value = "";
         this.form.style.display = "none";
-        requestAnimationFrame(() => this.render());
+        this.render();
       } else {
+        console.log("here");
       }
     });
   }
@@ -38,19 +41,22 @@ export default class TextArea {
     this.startPosition.y = this.textContainer.getBoundingClientRect().y;
   }
 
-  // requestanimationframe을 실시간을 계속 돌리지 않을 방법
-  // translateY를 이용해서 옮길 방법
+  // lerp 사용해서 가속도 구현
   render() {
-    this.startPosition.y = this.startPosition.y - 2;
+    this.velocity += this.accel;
+    console.log(rafId);
+    this.startPosition.y = this.startPosition.y - this.velocity;
     this.textContainer.style.transform = `translateY(${this.startPosition.y}px)`;
+    rafId = requestAnimationFrame(() => this.render());
     if (
       this.textContainer.getBoundingClientRect().y <
       this.textContainer.getBoundingClientRect().height
     ) {
+      this.velocity = 2;
       socket.emit("newText", this.textContainer.children[0].innerHTML);
-      this.textContainer.remove();
+      this.textContainer.children[0].remove();
       this.form.style.display = "flex";
+      cancelAnimationFrame(rafId);
     }
-    requestAnimationFrame(() => this.render());
   }
 }
