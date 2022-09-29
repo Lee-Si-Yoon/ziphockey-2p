21c43961d3d3bd9a97dd9b737956d89448462716
leesiyoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2022-09-29 11:49:39 +0900

    translateY로 변경

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,3 +1,20 @@
+import { gsap } from "gsap";
+import TextArea from "./text";
 import "../scss/styles.scss";
 
 const socket = io();
+
+const body = document.querySelector("body");
+
+window.onload = () => {
+  body.classList.remove("loading");
+  gsap.from(body, {
+    opacity: 0,
+    duration: 1,
+    ease: "Power3.easeOut",
+  });
+  const textarea = new TextArea(
+    document.getElementById("textAreaContainer"),
+    document.querySelector(".send__form")
+  );
+};
--- src/public/js/text.js
+++ src/public/js/text.js
@@ -2,29 +2,27 @@ const socket = io();
 
 const lerp = (a, b, n) => parseFloat((1 - n) * a + n + b).toFixed(2);
 
-let textStack = [];
 let posY;
 let accel = 1;
 
-class sender {
-  constructor() {
-    this.textContainer = document.getElementById("textAreaContainer");
-    this.form = document.querySelector(".send__form");
+export default class TextArea {
+  constructor(ulElement, formElement) {
+    this.textContainer = ulElement;
+    this.form = formElement;
+    this.startPosition = { x: 0, y: 0 };
     this.onSubmit(this.form);
-    this.sendBox();
   }
+
   onSubmit(element) {
     element.addEventListener("submit", (e) => {
       e.preventDefault();
-      let textChildrens = [
-        ...document.getElementById("textAreaContainer").children,
-      ];
-      // 새로 메세지를 보낼 경우
+      let textChildrens = [...this.textContainer.children];
       if (textChildrens.length === 0) {
+        // 새로 메세지를 보낼 경우
         this.createBox(e.target[0].value);
         e.target[0].value = "";
         this.form.style.display = "none";
-        this.sendBox(this.textContainer.children[0]);
+        requestAnimationFrame(() => this.render());
       } else {
       }
     });
@@ -33,30 +31,26 @@ class sender {
     const newBox = document.createElement("li");
     newBox.innerText = value;
     this.textContainer.appendChild(newBox);
-    this.textContainer.style.setProperty(
-      "--move-y",
-      `${window.innerHeight - newBox.getBoundingClientRect().height}px`
-    );
-    textStack.push(newBox);
-    posY = newBox.getBoundingClientRect().y;
+    this.textContainer.style.transform = `translateY(${
+      window.innerHeight - this.textContainer.getBoundingClientRect().height
+    }px)`;
+    this.startPosition.x = this.textContainer.getBoundingClientRect().x;
+    this.startPosition.y = this.textContainer.getBoundingClientRect().y;
   }
+
   // requestanimationframe을 실시간을 계속 돌리지 않을 방법
-  sendBox() {
-    posY = posY - 2 * accel;
-    accel += 0.01;
-    this.textContainer.style.setProperty("--move-y", `${posY}px`);
+  // translateY를 이용해서 옮길 방법
+  render() {
+    this.startPosition.y = this.startPosition.y - 2;
+    this.textContainer.style.transform = `translateY(${this.startPosition.y}px)`;
     if (
       this.textContainer.getBoundingClientRect().y <
-        this.textContainer.getBoundingClientRect().height &&
-      this.textContainer.children[0]
+      this.textContainer.getBoundingClientRect().height
     ) {
-      accel = 1;
       socket.emit("newText", this.textContainer.children[0].innerHTML);
-      this.textContainer.children[0].remove();
+      this.textContainer.remove();
       this.form.style.display = "flex";
     }
-    requestAnimationFrame(this.sendBox.bind(this));
+    requestAnimationFrame(() => this.render());
   }
 }
-
-new sender();
