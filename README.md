9a2987b9d82bbb8754e1339d2778a15c523ae96c
leesiyoon <76679207+Lee-Si-Yoon@users.noreply.github.com>
2022-11-20 15:57:16 +0900

    projcet cleanup

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 가제 비트윈
+# Ziphockey
 
 단거리에서 통신하는 핸드폰들
 
@@ -13,17 +13,16 @@ using
 ## 해결해야할 것들
 
 - [ ] 세팅 방법 노션에 정리해두기
-- [ ] 헤로쿠 말고 12월까지 무료로 호스팅 할수 있는 곳, 파이어베이스 → 파이어베이스 functions은 유료
-- [x] 모든 루트들을 볼 수 있는 홈페이지
-- [x] 입출력 matter.js 없이 먼저
-- [x] matter.js로 연동
-- [ ] chat.js에서 mapHTML에서 body 삭제하는 방법
-- [ ] chat.js에서 x위치 연동 시키기 + 좀더 방사형으로 뻗어나가게
-- [x] 모바일로 보면 fps가 40이하로 고정되는 문제 -> 개체수 조절
-- [ ] light class랑 bypass 찢는 방법 고안
-- [ ] socket emit하는거 리펙토링
-- [ ] pug랑 css도 모듈화 고안
-- [ ] 조도 센서 신호등 처럼 색상 추가
+- [ ] 헤로쿠 말고 12월까지 무료로 호스팅 할수 있는 곳
+  - [x] 파이어베이스 → 파이어베이스 functions은 유료
+  - [ ] back4app은?
+- [x] socket상에서 ready 할때 숫자 맞추도록
+- [x] 컨트롤러 손보기
+- [x] 벽 충돌 손보기
+- [ ] 소켓과 연동
+  - [x] 소켓 파일을 따로 만들어서 export로 관리
+- [x] 벽 생기는 거 손보기 - 1플레이어는 오른쪽 벽 없기, 2플은...
+- [ ] 공 생기는 조건
 
 ## [링크](https://between-gz.herokuapp.com/)
 
@@ -46,3 +45,5 @@ using
 https://github.com/nomadcoders/wetube-reloaded
 https://socket.io/docs/v4/rooms/
 zoom-clone
+
+- https://github.com/danielszabo88/CapsuleSoccer/blob/master/05%20-%20Adding%20socket.io%20Rooms/server.js
--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,22 +1,18 @@
+// const socket = io();
 import { gsap } from "gsap";
-import { v4 as uuidv4 } from "uuid";
 import "../scss/styles.scss";
-const socket = io();
 
 const body = document.querySelector("body");
-const homeContainer = document.querySelector(".home");
-const newRoomContainer = document.querySelector(".newroom");
-const readyContainer = document.querySelector(".ready");
 
-const enterNewRoom = document.getElementById("enterNewRoom");
-const enterRoomForm = document.getElementById("enterRoomForm");
-const backToHome = newRoomContainer.querySelector(".back");
-const newRoomForm = document.getElementById("newRoomForm");
-const readyNumber = document.getElementById("ready__number");
-const readyRoomName = document.getElementById("ready__roomName");
-const readyButton = document.getElementById("ready__button");
-
-let nameOfRoom;
+/**
+ * 페이지 숨기고 보여줄 때 사용
+ * @param {HTMLElement} hide 숨길 것
+ * @param {HTMLElement} show 보여줄 것
+ */
+export function hideAndShow(hide, show) {
+  hide.classList.add("hidden");
+  show.classList.remove("hidden");
+}
 
 window.onload = () => {
   body.classList.remove("loading");
@@ -26,65 +22,4 @@ window.onload = () => {
     ease: "Power3.easeOut",
   });
 
-  enterNewRoom.addEventListener("click", () => {
-    newRoomContainer.classList.remove("hidden");
-    homeContainer.classList.add("hidden");
-  });
-  backToHome.addEventListener("click", () => {
-    homeContainer.classList.remove("hidden");
-    newRoomContainer.classList.add("hidden");
-  });
-
-  enterRoomForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const inputs = enterRoomForm.querySelectorAll("input");
-    socket.emit("enterRoom", inputs[0].value);
-    if (inputs[0].value !== "" && inputs[0].value !== "enter room id") {
-      readyContainer.classList.remove("hidden");
-      homeContainer.classList.add("hidden");
-      readyRoomName.innerText = `${inputs[0].value}`;
-    }
-  });
-
-  newRoomForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const { value } = newRoomForm.querySelector(".room__capacity__number");
-    nameOfRoom = uuidv4().slice(0, 3) + value;
-    socket.emit("enterRoom", nameOfRoom);
-    readyContainer.classList.remove("hidden");
-    newRoomContainer.classList.add("hidden");
-    readyRoomName.innerText = nameOfRoom;
-    readyNumber.innerText = `1/${value}`;
-  });
-
-  socket.on("welcome", (roomName, count) => {
-    const capacity = roomName.slice(-1);
-    readyNumber.innerText = `${count}/${capacity}`;
-    if (+count == +capacity) {
-      readyButton.innerHTML = "Are you ready?";
-      function onClick() {
-        readyButton.classList.remove("orange");
-        readyButton.classList.add("green");
-        readyButton.innerHTML = "Ready!";
-        socket.emit("getReady", roomName, count);
-        readyButton.removeEventListener("click", onClick);
-      }
-      readyButton.addEventListener("click", onClick);
-    }
-  });
-
-  socket.on("ready", (roomName, count) => {
-    const capacity = roomName.slice(-1);
-    readyNumber.innerText = `${count}/${capacity}`;
-    if (+count == +capacity) {
-      readyButton.innerHTML = "Are you ready?";
-      function onClick() {
-        readyButton.classList.remove("orange");
-        readyButton.classList.add("green");
-        readyButton.innerHTML = "Ready!";
-        readyButton.removeEventListener("click", onClick);
-      }
-      readyButton.addEventListener("click", onClick);
-    }
-  });
 };
